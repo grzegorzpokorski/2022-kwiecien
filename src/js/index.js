f1176657a1c910a7f9421e355dd6e210a1c35cc9
unknown <mr.pokorski@gmail.com>
2022-04-06 15:48:20 +0200

    added navbar background chandling when its collapse

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,32 @@
 import 'bootstrap';
 
-window.addEventListener('scroll',(e)=>{
-	const nav = document.querySelector('#menu');
+const menu = document.querySelector('#menu');
+const navbarNav = menu.querySelector('#navbarNav');
+
+window.addEventListener('scroll', (e) => {
 	if(window.pageYOffset > 0){
-		nav.classList.add("bg-white");
-		nav.classList.add("shadow-sm");
-		nav.classList.remove("py-md-2");
+		menu.classList.add('bg-white');
+		menu.classList.add('shadow-sm');
+		menu.classList.remove('py-md-2');
 	}else{
-		nav.classList.remove("bg-white");
-		nav.classList.remove("shadow-sm");
-		nav.classList.add("py-md-2");
+		if(!navbarNav.classList.contains('show')){
+			menu.classList.remove('bg-white');
+			menu.classList.remove('shadow-sm');
+			menu.classList.add('py-md-2');	
+		}
 	}
 });
+
+menu.addEventListener('click', ({ target }) => {
+	if(target.classList.contains('navbar-toggler') || target.classList.contains('navbar-toggler-icon')){
+		if(menu.classList.contains('bg-white')){
+			if(window.pageYOffset == 0){
+				menu.classList.remove('bg-white');
+				menu.classList.remove('shadow-sm');
+			}
+		}else{
+			menu.classList.add('bg-white');
+			menu.classList.add('shadow-sm');
+		}
+	}
+})
