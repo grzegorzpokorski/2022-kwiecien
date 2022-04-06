76b59d8f150dfc911a395abe33e5fca34f71ac8b
unknown <mr.pokorski@gmail.com>
2022-04-06 14:49:52 +0200

    added navbar, footer and more

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ### Pages:
 * [index](https://grzegorzpokorski.github.io/2022-kwiecien/index.html)
+* [archive](https://grzegorzpokorski.github.io/2022-kwiecien/archive.html)
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 import 'bootstrap';
+
+window.addEventListener('scroll',(e)=>{
+	const nav = document.querySelector('#menu');
+	if(window.pageYOffset > 0){
+		nav.classList.add("bg-white");
+		nav.classList.add("shadow-sm");
+		nav.classList.remove("py-md-2");
+	}else{
+		nav.classList.remove("bg-white");
+		nav.classList.remove("shadow-sm");
+		nav.classList.add("py-md-2");
+	}
+});
